5c0e38b4b0a26ac5d7b18f40799b68fa78fa79f3
DennyVamilion <109918366+DennyVamilion@users.noreply.github.com>
2022-07-24 20:01:13 +0200

    Add files via upload

---
+++ main.c
@@ -0,0 +1,35 @@
+//Programmer: Zer02c
+//Date: 23.07.2022
+// this script is made for the HC-SR04 Ultrasonic Sensor Module
+
+#include "pico/stdlib.h"
+
+#define LOW 0
+#define HIGH 1
+
+int main(){
+    stdio_init_all();
+
+    int triggerpin = 2;
+    int echopin = 3;
+    int messung = 0;
+    int ergebnis = 0;
+
+    gpio_init(triggerpin);
+    gpio_set_dir(triggerpin, GPIO_OUT);
+
+    gpio_init(echopin);
+    gpio_set_dir(echopin, GPIO_IN);
+
+    while (true)
+    {
+        gpio_put(triggerpin, LOW);
+        sleep_ms(2);
+        gpio_put(triggerpin, HIGH);
+        sleep_ms(10);
+        gpio_put(triggerpin, LOW);
+
+        messung = 
+    }
+    
+}
